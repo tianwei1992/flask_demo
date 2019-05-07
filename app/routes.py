d81da973004aed06947996dc32ca1a1a48d357aa
grace <tianweigrace@qq.com>
2019-05-07 12:11:31 +0800

    Add forms on login page

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
 from flask import render_template
+
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -17,3 +20,9 @@ def index():
     user = {'username': 'Miguel'}
     return render_template('index.html', title='Home', user=user, posts=posts)    # 注意模板路径没有包含templates目录
 
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)

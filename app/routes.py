a8769cf2a65b7f1267fb2885eeb7efd2f729049c
grace <tianweigrace@qq.com>
2019-05-07 17:57:31 +0800

    Add methods to deal with POST requests, including flashed_messages and redirect

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 
 from app import app
 from app.forms import LoginForm
@@ -21,8 +21,12 @@ def index():
     return render_template('index.html', title='Home', user=user, posts=posts)    # 注意模板路径没有包含templates目录
 
 
-
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    """只有post且验证成功才是rediret首页，不成功的post和get都是redirect到login页"""
     form = LoginForm()
+    if form.validate_on_submit():    # if request_method == 'POST'..
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data)) # generate flashed_messages
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)

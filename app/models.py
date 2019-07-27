37c651867a1082da1cec7aac1f09ea42e1d56f9a
grace <tianweigrace@qq.com>
2019-07-27 14:18:37 +0800

    Realize user avatar.

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from hashlib import md5
 
 from app import db, login    # login = LoginManager(app)
 
@@ -12,6 +13,12 @@ class User(UserMixin, db.Model):    # mix现成的is_authenticated方法等
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+
+    def avatar(self, size):
+        """头像"""
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)    

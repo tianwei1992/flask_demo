87aa171f8d67e23d85ce1024da23855147a3397f
grace <tianweigrace@qq.com>
2019-08-21 19:58:47 +0800

    Integrate shell commands to flask.

--- microblog.py
+++ microblog.py
@@ -1,5 +1,6 @@
-from app import app, db    # 这里app是app.__init__
+from app import app, db, cli    # 这里app是app.__init__
 from app.models import User, Post    # 这里app是app目录
+
 
 @app.shell_context_processor   # 无需导入就可在flask db中使用以下实例
 def make_shell_context():
